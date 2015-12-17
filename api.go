9fc4581c0596d65adf1cdb026bd63a96efb805b2
Antonio Murdaca <runcom@redhat.com>
2015-12-17 11:32:00 +0100

    Revert "more interface fixes"
    This reverts commit 6afca60b0888a9fa552eaeddbd2ac2e4bffbbaff.

--- api.go
+++ api.go
@@ -21,7 +21,7 @@ const (
 
 type Request authorization.Request
 
-type Response authorization.Response
+type Response authorization.Request
 
 // Handler forwards requests and responses between the docker daemon and the plugin.
 type Handler struct {
@@ -43,7 +43,7 @@ func (h *Handler) initMux() {
 	})
 
 	h.handle(reqPath, func(req Request) Response {
-		return h.plugin.AuthZRequest(req)
+		return h.plugin.AuthZApiRequest(req)
 	})
 
 	h.handle(resPath, func(req Request) Response {
